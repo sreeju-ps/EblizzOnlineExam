6e9d114fcd51049ade4956de51ff20327a9ff6cf
Dev <Dev>
2016-09-20 10:23:39 +0530

    initial commit

---
+++ eblizzOnlineExam/src/eblizzonlineexam/EblizzOnlineExam.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eblizzonlineexam;
+
+/**
+ *
+ * @author Dev
+ */
+public class EblizzOnlineExam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
